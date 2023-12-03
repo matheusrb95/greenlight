4884875e071ca8aaba3d5dc69c96bee7e3a3241a
Matheus <matheusrenan.95@gmail.com>
2023-12-03 15:34:04 -0300

    feat: CRUD operation

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -16,7 +16,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	params := httprouter.ParamsFromContext(r.Context())
 
 	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	if err != nil {
 		return 0, errors.New("invalid id parameter")
 	}
 
